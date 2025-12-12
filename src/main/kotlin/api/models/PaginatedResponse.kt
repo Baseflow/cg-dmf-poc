bcbd2ce86388c6b8776ae781860c0f6ed9982f0c
Derk-Jan Hartman <derk-jan@baseflow.com>
2025-12-12 11:40:10 +0100

    Mark PaginatedResponse as data class

--- src/main/kotlin/api/models/PaginatedResponse.kt
+++ src/main/kotlin/api/models/PaginatedResponse.kt
@@ -3,7 +3,7 @@ package com.baseflow.api.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal class PaginatedResponse<T>(
+data class PaginatedResponse<T>(
     val count: Int,
     val next: String? = null,
     val previous: String? = null,
