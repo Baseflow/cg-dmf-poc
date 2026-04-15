9d171f9fc4ab24a9d52bac384310a6f9239c763c
Jan-Derk de Vries <janderk@baseflow.com>
2026-04-15 11:40:06 +0200

    feat(auth): remove hardcoded user from AppSidebar

--- frontend/admin-portal/components/app-sidebar.tsx
+++ frontend/admin-portal/components/app-sidebar.tsx
@@ -69,11 +69,6 @@ function DockerIcon() {
 }
 
 const data = {
-  user: {
-    name: "Baseflow",
-    email: "hello@baseflow.com",
-    avatar: <BaseflowAvatar />,
-  },
   navMain: [
     {
       title: "Lifecycle",
@@ -166,7 +161,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavSecondary items={data.NavSecondary} className="mt-auto"/>
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser />
       </SidebarFooter>
     </Sidebar>
   )
