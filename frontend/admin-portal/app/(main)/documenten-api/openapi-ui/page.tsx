99ed973a3e279287d840c9a7722a53b8f6b15ab3
Laurens Jan <laurensjan@baseflow.com>
2026-05-06 15:01:14 +0200

    add dockerfille

--- frontend/admin-portal/app/(main)/documenten-api/openapi-ui/page.tsx
+++ frontend/admin-portal/app/(main)/documenten-api/openapi-ui/page.tsx
@@ -1,5 +1,5 @@
 import { ApiReference } from "./api-reference"
-
+export const dynamic = "force-dynamic"
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col overflow-hidden">
