1d86423a821b012285c668ba15966967a57b4e13
Jan-Derk de Vries <janderk@baseflow.com>
2026-05-27 13:24:39 +0200

    Updated documentation for Swagger UI

--- frontend/admin-portal/app/(main)/documenten-api/openapi-ui-swagger/page.tsx
+++ frontend/admin-portal/app/(main)/documenten-api/openapi-ui-swagger/page.tsx
@@ -1,5 +1,7 @@
 import { SwaggerUiReference } from "./swagger-ui-reference"
 
+export const dynamic = "force-dynamic"
+
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col overflow-hidden">
