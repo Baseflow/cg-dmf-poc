5e274489bf3ba64891e65ea9bd6bf76d2d6dda57
Maurits van Beusekom <maurits@vnbskm.nl>
2026-06-03 09:26:52 +0200

    Fix formatting description

--- src/main/kotlin/api/wopi/routes/WopiApiRoutes.kt
+++ src/main/kotlin/api/wopi/routes/WopiApiRoutes.kt
@@ -237,7 +237,9 @@ fun Route.wopiApiRoutes() {
                         response(200) { description = "Success." }
                         response(400) {
                             description =
-                                "Bad request. This error is returned when the request contains an X-WOPI-MaxExpectedSize header that does not contain a valid 32-bit unsigned integer value."
+                                "Bad request. This error is returned when the request contains an " +
+                                "X-WOPI-MaxExpectedSize header that does not contain a valid 32-bit unsigned " +
+                                "integer value."
                         }
                         response(401) { description = "Invalid access token." }
                         response(404) { description = "Resource not found or user unauthorized." }
@@ -408,7 +410,7 @@ private suspend fun RoutingContext.getFileContents() {
             call.respondProblem(
                 HttpStatusCode.BadRequest,
                 badRequest(
-                    "The X-WOPI-MaxExpectedSize does not contain a valid 32-bit unsigned integer value.",
+                    "The X-WOPI-MaxExpectedSize header does not contain a valid 32-bit unsigned integer value.",
                     call.request.path(),
                 ),
             )
